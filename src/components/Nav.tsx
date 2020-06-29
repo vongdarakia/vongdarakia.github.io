fe86693699ffbc1ce96f7799dc09912055d78fcf
Akia Vongdara <vongdarakia@gmail.com>
2020-06-28 20:56:05 -0700

    Add some spacing styles for Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Link } from "gatsby";
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+    text-align: right;
+    padding: 24px;
+
+    a:not(:first-child):not(:last-child) {
+        margin: 0 16px;
+    }
+`;
 
 export const Nav = () => {
     return (
-        <nav>
+        <StyledNav>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/blog">Blog</Link>
-        </nav>
+        </StyledNav>
     );
 };
